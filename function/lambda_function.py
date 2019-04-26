b21ef55398d64f4ef317ee88ef89043d7ccba5df
Fayzullaev <nfayzullaev@informatica.com>
2019-04-26 11:29:42 -0400

    Modified JSON post message to correspond to Microsoft Teams format

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -3,39 +3,15 @@ import time
 import feedparser
 import boto3
 import requests
-import datetime
-import json
-
-from bs4 import BeautifulSoup
-
-from datetime import timezone
 from io import BytesIO
 
 HOOK_URL = os.environ['BOT_URL']
 TABLE_NAME = os.environ['TABLE_NAME']
-FEEDS_TABLE = os.environ['FEEDS_CONFIG']
-STREAM = os.environ['KINESIS_STREAM']
-
 DB = boto3.resource('dynamodb')
-KINESIS = boto3.client('firehose')
-DEFAULT_FEEDS = [
-    {'url': 'https://aws.amazon.com/new/feed/', 'source': 'AWS', 'category': 'news'},
-    {'url': 'https://aws.amazon.com/security/security-bulletins/feed/', 'source': 'AWS', 'category': 'security'}    
+FEEDS = [
+    'https://aws.amazon.com/new/feed/',
+    'https://aws.amazon.com/security/security-bulletins/feed/'
 ]
-
-def load_feeds():
-    table = DB.Table(FEEDS_TABLE)
-    response = table.scan()
-    data = response['Items']
-    while response.get('LastEvaluatedKey'):
-        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        data.extend(response['Items'])
-    print("Loaded feeds: ", data)
-    return data
-
-feeds = load_feeds()
-if len(feeds) <= 0:
-    feeds = DEFAULT_FEEDS
 
 def check_items(keys, items):
     if len(keys) > 0:
@@ -63,60 +39,37 @@ def commit_items(dynamodb_items):
         )
         print('DynamoDB write capacity used: ', response['ConsumedCapacity'])
 
-def clean_text(text):
-    text = BeautifulSoup(text).get_text()
-    text = text.replace('\\n', '\n')
-    text = text.replace('\xa0', ' ')
-    return text
-
-def parse_date(date):
-    date = date.replace(' +0000', '').replace(' -0000', '')
-    date = date.replace(' Z', '')
-    dt = datetime.datetime.strptime(date, "%a, %d %b %Y %H:%M:%S").isoformat()
-    return dt
-    
 def load_new_items():
     items = {}
-    for feed in feeds:
+    for feed in FEEDS:
         print(feed)
         try:
-            resp = requests.get(feed['url'], timeout=10.0)
+            resp = requests.get(feed, timeout=10.0)
         except requests.ReadTimeout:
             print("Timeout when reading RSS %s", feed)
             continue
 
+        # Put it to memory stream object universal feedparser
         content = BytesIO(resp.content)
+
+        # Parse content
         news_feed = feedparser.parse(content)
 
+        #news_feed = feedparser.parse(feed)
         keys = []
         id_dedup = {}
-        epoch_time = int(time.time()) + 2592000 * 36 # 3 years
+        epoch_time = int(time.time()) + 2592000
         for entry in news_feed['entries']:
-            title = entry['title']
-            summary = clean_text(entry['summary'])
-            dt = parse_date(entry['published'])
-
-            if len(summary) <= 0:
-                summary = title
-
-            id = feed['source'] + " " + title.lower()
+            id = entry['title_detail']['value'].lower()
             if id in id_dedup:
                 continue
-
             id_dedup[id] = id
             keys.append({'id': id})
             items[id] = {
                 'id': id,
-                'title': title,
-                '@timestamp': dt,
-                'source': feed['source'],
-                'category': feed['category'],
-                'summary': summary,
                 'expire': epoch_time,
                 'message': entry['title_detail']['value'] + ': ' +  entry['link']
             }
-
-
             if len(keys) == 20:
                 items = check_items(keys, items)
                 keys = []
@@ -135,10 +88,16 @@ def load_new_items():
 
 
 def post_message(content, tries=0):
+    msg = {
+      "@context": "https://schema.org/extensions",
+      "@type": "MessageCard",
+      "text": content
+    }
+    
     if tries > 5:
         print("DEBUG: Retried 6 times. Giving up")
         return False
-    response = requests.post(url=HOOK_URL, json={"Content": content})
+    response = requests.post(url=HOOK_URL, json=msg)
     if response.status_code != 200:
         print("DEBUG: Content: ", content)
         print("DEBUG: Response: ", response)
@@ -147,33 +106,24 @@ def post_message(content, tries=0):
         time.sleep(3)
         print("DEBUG: Retrying")
         return post_message(content, tries + 1)
-    else:
-        time.sleep(1)
+    else: time.sleep(1)
     return True
-
-def publish(message):
-    if len(STREAM) > 0:
-        KINESIS.put_record(
-            DeliveryStreamName=STREAM,
-            Record={'Data': message.encode('utf-8')}
-        )
 
 def lambda_handler(event, context):
     new_messages = load_new_items()
     status = False
     content = ""
-    if len(new_messages) >= 500:
-        response = requests.post(url=HOOK_URL, json={"Content": "Too many new messages..."})
+    if len(new_messages) >= 60:
+        response = requests.post(url=HOOK_URL, json={"text": "Too many new messages... Check the site: https://aws.amazon.com/new. "})
         print(response)
         return
 
-    print("Posting {} news.".format(len(new_messages)))
     for __, message in new_messages.items():
-        publish(json.dumps(message))
-        content = '[' + message['source'] + '] ' + message['message']
-        if post_message(content) != True:
-            print("DEBUG: Failed to send: ", message['id'])
-            status = True
+        content = message['message']
+        # print(content)
+		if post_message(content) != True:
+				print("DEBUG: Failed to send: ", message['id'])
+				status = True
     
     if status:
         raise Exception("At least one news wasn't sent!")
